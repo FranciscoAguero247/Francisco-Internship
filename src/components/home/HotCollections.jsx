e17d94839dda94aa57c19d60073882f320bc77b8
FranciscoAguero247 <blast255@live.com>
2026-05-06 11:25:51 -0600

    implemented skeleton loading in hot collection section

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -4,18 +4,24 @@ import axios from "axios";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import Skeleton from "../UI/Skeleton.jsx";
+
 
 const HotCollections = () => {
 
   const [hotCollections, setHotCollections] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     axios
       .get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
       .then((response) => {
         setHotCollections(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
+        setLoading(false);
       });
   }, []);
 
@@ -53,6 +59,27 @@ const HotCollections = () => {
             </div>
           </div>
         <div className="slider-container">
+          {loading ? (
+            <div className="d-flex">
+              {new Array(4).fill(0).map((_, index) => (
+                <div className="col-lg-3 col-md-6 col-sm-6 col-sm-12" key={index}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <Skeleton width={100} height={200} />
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Skeleton width={50} height={50} borderRadius={50} />
+                    </div>
+                    <div className="nft_coll_info">
+                      <Skeleton width={100} height={20} />
+                      <br />
+                      <Skeleton width={60} height={15} />
+                      </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
           <Slider {...settings}>
           {hotCollections.map((data, index) =>(
             <div className="px-2" key={index}>
@@ -78,7 +105,8 @@ const HotCollections = () => {
             </div>
             ))}
             </Slider>
-            </div>
+          )}
+          </div>
         </div>
       </div>
     </section>
