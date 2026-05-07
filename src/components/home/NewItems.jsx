42dfdf94253c1c3b52a6ba609140331bd1ec5bce
FranciscoAguero247 <blast255@live.com>
2026-05-07 14:47:30 -0600

    conected api and made time counter

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+
+ const CountDownTimer = ({expiryDate}) => {
+      const[timeLeft, setTimeLeft] = useState("");
+
+        useEffect(() => {
+        const calculateTime = () => {
+          const currentTime = new Date().getTime();
+          const timeDifference = expiryDate - currentTime;
+
+          if (timeDifference <= 0) {
+            setTimeLeft("Expired");
+            return;
+          }
+
+          const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+          const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+          setTimeLeft(`${hours}h ${minutes}m ${seconds}s`);
+        };
+
+        calculateTime();
+        const timer = setInterval(calculateTime, 1000);
+
+        return () => clearInterval(timer);
+      }, [expiryDate]);
+
+      return <div className="de_countdown">{timeLeft}</div>;
+    };
 
 const NewItems = () => {
+  
+    const [newItems, setNewItems] = useState([]);
+    // const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+      axios
+        .get("https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems")
+        .then((response) => {
+          setNewItems(response.data);
+          // setLoading(false);
+      })
+        .catch((error) => {
+          console.log(error);
+          // setLoading(false);
+        });
+    }, []);
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,22 +59,26 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+
+          {newItems.map((data, index) =>(
+
+
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link
-                    to="/author"
+                    to={`/author/${data.authorId}`}
                     data-bs-toggle="tooltip"
                     data-bs-placement="top"
-                    title="Creator: Monica Lucas"
+                    title={`Creator: ${data.authorId}`}
                   >
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={data.authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
+                { data.expiryDate && (
+                  <CountDownTimer expiryDate={data.expiryDate} />
+                )}
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
                     <div className="nft__item_buttons">
@@ -49,9 +98,9 @@ const NewItems = () => {
                     </div>
                   </div>
 
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${data.nftId}`}>
                     <img
-                      src={nftImage}
+                      src={data.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
@@ -59,17 +108,21 @@ const NewItems = () => {
                 </div>
                 <div className="nft__item_info">
                   <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{data.title}</h4>
                   </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
+                  <div className="nft__item_price">{data.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>69</span>
+                    <span>{data.likes}</span>
                   </div>
                 </div>
               </div>
             </div>
+
+
           ))}
+
+
         </div>
       </div>
     </section>
