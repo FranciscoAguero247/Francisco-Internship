b68ecc7be703782190d197c5e9935f726c7a9745
FranciscoAguero247 <blast255@live.com>
2026-05-13 15:22:29 -0600

    API data retrived

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+
+const CountDownTimer = ({expiryDate}) => {
+      const[timeLeft, setTimeLeft] = useState("");
+
+        useEffect(() => {
+        const calculateTime = () => {
+          const currentTime = new Date().getTime();
+          const timeDifference = expiryDate - currentTime;
+
+          if (timeDifference <= 0) {
+            setTimeLeft("Expired");
+            return;
+          }
+
+          const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+          const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+          setTimeLeft(`${hours}h ${minutes}m ${seconds}s`);
+        };
+
+        calculateTime();
+        const timer = setInterval(calculateTime, 1000);
+
+        return () => clearInterval(timer);
+      }, [expiryDate]);
+
+      return <div className="de_countdown">{timeLeft}</div>;
+    };
 
 const ExploreItems = () => {
+
+  const [exploreItems, setExploreItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => { 
+    
+    axios
+      .get("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore")
+      .then((response) => {
+        setExploreItems(response.data);
+        setLoading(false);
+        })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+      }, []);
+
   return (
     <>
       <div>
@@ -14,7 +62,8 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
+
+      {exploreItems.map((exploreItem, index) => (
         <div
           key={index}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -23,15 +72,17 @@ const ExploreItems = () => {
           <div className="nft__item">
             <div className="author_list_pp">
               <Link
-                to="/author"
+                to={`/author/${exploreItem.authorId}`}
                 data-bs-toggle="tooltip"
                 data-bs-placement="top"
               >
-                <img className="lazy" src={AuthorImage} alt="" />
+                <img className="lazy" src={exploreItem.authorImage} alt="" />
                 <i className="fa fa-check"></i>
               </Link>
             </div>
-            <div className="de_countdown">5h 30m 32s</div>
+            { exploreItem.expiryDate && (
+                  <CountDownTimer expiryDate={exploreItem.expiryDate} />
+            )}
 
             <div className="nft__item_wrap">
               <div className="nft__item_extra">
@@ -52,22 +103,23 @@ const ExploreItems = () => {
                 </div>
               </div>
               <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
+                <img src={exploreItem.nftImage} className="lazy nft__item_preview" alt="" />
               </Link>
             </div>
             <div className="nft__item_info">
               <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
+                <h4>{exploreItem.title}</h4>
               </Link>
-              <div className="nft__item_price">1.74 ETH</div>
+              <div className="nft__item_price">{exploreItem.price} ETH</div>
               <div className="nft__item_like">
                 <i className="fa fa-heart"></i>
-                <span>69</span>
+                <span>{exploreItem.likes}</span>
               </div>
             </div>
           </div>
         </div>
       ))}
+
       <div className="col-md-12 text-center">
         <Link to="" id="loadmore" className="btn-main lead">
           Load more
