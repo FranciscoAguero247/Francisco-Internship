c31de5efc1be049c6fb8e0571699761be08b7a06
FranciscoAguero247 <blast255@live.com>
2026-05-13 20:38:01 -0600

    reusable NFT card component

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -4,6 +4,8 @@ import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 import CountDownTimer from "../UI/CountDownTimer.jsx";
+import NFTCard from "../UI/NFTCard.jsx";
+
 
 const ExploreItems = () => {
 
@@ -36,60 +38,7 @@ const ExploreItems = () => {
       </div>
 
       {exploreItems.map((exploreItem, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to={`/author/${exploreItem.authorId}`}
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={exploreItem.authorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            { exploreItem.expiryDate && (
-                  <CountDownTimer expiryDate={exploreItem.expiryDate} />
-            )}
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={exploreItem.nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>{exploreItem.title}</h4>
-              </Link>
-              <div className="nft__item_price">{exploreItem.price} ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>{exploreItem.likes}</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <NFTCard key={index} data={exploreItem} className="col-lg-3 col-md-6 col-sm-6 col-sm-12" />
       ))}
 
       <div className="col-md-12 text-center">
